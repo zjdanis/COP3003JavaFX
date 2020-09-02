c8ac14215bfb17494f01a7c031ec21328cdbab86
zjdanis <zdanis4585@eagle.fgcu.edu>
2020-09-02 18:30:16 -0400

    Commited

---
+++ Sep1Demo/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args){
+        Person vlad = new Person();
+        vlad.setLiving(true);
+        System.out.println(vlad.getLiving());
+    }
+}
---
+++ Sep1Demo/src/Person.java
@@ -0,0 +1,14 @@
+public class Person {
+
+    private boolean living;
+    private int age;
+    private String eyeColor;
+
+    public void setLiving(boolean l){
+        living = l;
+    }
+
+    public boolean getLiving(){
+        return living;
+    }
+}
