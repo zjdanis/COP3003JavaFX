ef13fd80ded512881a1ef728e7a04359c403faa5
zjdanis <zdanis4585@eagle.fgcu.edu>
2020-09-19 17:22:06 -0400

    Committed

---
+++ DemoSep9/src/com/company/Main.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        final String JDBC_DRIVER = "org.h2.Driver";
+        //if h2 does not populate,
+
+        final String DB_URL = "jdbc:h2:./res/HRdb";
+
+        //  Database credentials
+        final String USER = "";
+        final String PASS = "";
+        Connection conn = null;
+        Statement stmt = null;
+
+        try {
+            // STEP 1: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 2: Open a connection
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //STEP 3: Execute a query
+            stmt = conn.createStatement();
+
+            String sql = "SELECT * FROM JOBS";
+
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+
+            // STEP 4: Clean-up environment
+            stmt.close();
+            conn.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Practice/src/io/github/zjdanis/Main.java
@@ -0,0 +1,10 @@
+package io.github.zjdanis;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	int [] y = new int[5];// write your here
+        System.out.println(y);
+    }
+}
